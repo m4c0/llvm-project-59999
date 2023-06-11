d52c43163e8d82e74e8ad859620d9eccf5be12ca
Eduardo Costa <m4c0@users.noreply.github.com>
2023-06-11 10:20:00 +0100

    remove extra flags from build

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
-clang++ -std=c++20 -fmodules -O3 -fprebuilt-module-path=. --precompile -o .\Module-Reflector.pcm .\Source\Engine\Module\Reflector.cppm 
-clang++ -std=c++20 -fmodules -O3 -fprebuilt-module-path=. --precompile -o .\Module-ModuleEntry.pcm .\Source\Engine\Module\ModuleEntry.cppm
-clang++ -std=c++20 -fmodules -O3 -fprebuilt-module-path=. --precompile -o .\Module.pcm .\Source\Engine\Module\Module.cppm
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Module-Reflector.pcm .\Source\Engine\Module\Reflector.cppm 
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Module-ModuleEntry.pcm .\Source\Engine\Module\ModuleEntry.cppm
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Module.pcm .\Source\Engine\Module\Module.cppm
 
-clang++ -std=c++20 -fmodules -O3 -fprebuilt-module-path=. --precompile -o .\Object-Reflector.pcm .\Source\Engine\Object\Reflector.cppm 
-clang++ -std=c++20 -fmodules -O3 -fprebuilt-module-path=. --precompile -o .\Object-NObject.pcm .\Source\Engine\Object\NObject.cppm 
-clang++ -std=c++20 -fmodules -O3 -fprebuilt-module-path=. --precompile -o .\Object.pcm .\Source\Engine\Object\Object.cppm
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Object-Reflector.pcm .\Source\Engine\Object\Reflector.cppm 
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Object-NObject.pcm .\Source\Engine\Object\NObject.cppm 
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Object.pcm .\Source\Engine\Object\Object.cppm
 
-clang++ -std=c++20 -fmodules -O3 -fprebuilt-module-path=. --precompile -o .\World.pcm .\Source\Engine\World\World.cppm
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\World.pcm .\Source\Engine\World\World.cppm
