2780f4ac0ba62a71838f94625ae1951fd0a1eb91
Eduardo Costa <m4c0@users.noreply.github.com>
2023-06-11 11:18:11 +0100

    flatten source dir

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 del *.pcm
 
-clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Module-Reflector.pcm .\Source\Engine\Module\Reflector.cppm 
-clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Module.pcm .\Source\Engine\Module\Module.cppm
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Module-Reflector.pcm .\Module-Reflector.cppm 
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Module.pcm .\Module.cppm
 
-clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Object-Reflector.pcm .\Source\Engine\Object\Reflector.cppm 
-clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Object.pcm .\Source\Engine\Object\Object.cppm
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Object-Reflector.pcm .\Object-Reflector.cppm 
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\Object.pcm .\Object.cppm
 
-clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\World.pcm .\Source\Engine\World\World.cppm
+clang++ -std=c++20 -fprebuilt-module-path=. --precompile -o .\World.pcm .\World.cppm
